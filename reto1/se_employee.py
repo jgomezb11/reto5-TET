de5a0a116781032a99f8366f86a05fe090916ec4
Julian Gomez Benitez <60365681+jgomezb11@users.noreply.github.com>
2023-06-02 22:40:18 -0500

    Update se_employee.py

--- reto1/se_employee.py
+++ reto1/se_employee.py
@@ -1,13 +1,26 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 class MRNumSEByEmployee(MRJob):
 
     def mapper(self, _, line):
-        idemp, sector, salary, year = line.split(',')
-        yield (idemp, sector), 1
+        idemp, sector, _, _ = line.split(',')
+        yield (idemp, sector), None
 
     def reducer(self, keys, values):
-        yield keys[0], sum(values)
+        yield keys[0], keys[1]
+
+    def reducer_count_sectors(self, idemp, sectors):
+        unique_sectors = len(set(sectors))
+        yield idemp, unique_sectors
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer),
+            MRStep(reducer=self.reducer_count_sectors)
+        ]
 
 if __name__ == '__main__':
     MRNumSEByEmployee.run()
+
